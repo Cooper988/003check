071ecf7865b34777ad401c0b038d707e96b0058c
hbstarjason <hbstarjason@gmail.com>
2020-07-17 01:42:32 +0800

    test two account again again

--- checkin2.py
+++ checkin2.py
@@ -11,27 +11,27 @@ cookie = os.environ["COOKIE"]
 
 cookie2 = os.environ["COOKIE2"]
 
-def start():    
+def start2():    
     url= "https://glados.rocks/api/user/checkin"
     url2= "https://glados.rocks/api/user/status"
     referer = 'https://glados.rocks/console/checkin'
-    checkin = requests.post(url,headers={'cookie2': cookie2 ,'referer': referer })
-    state =  requests.get(url2,headers={'cookie2': cookie2 ,'referer': referer})
+    checkin2 = requests.post(url,headers={'cookie2': cookie2 ,'referer': referer })
+    state2 =  requests.get(url2,headers={'cookie2': cookie2 ,'referer': referer})
 
-    if 'message' in checkin.text:
-        mess = checkin.json()['message']
-        time = state.json()['data']['leftDays']
-        time = time.split('.')[0]
-        print(time)
+    if 'message2' in checkin.text:
+        mess2 = checkin2.json()['message']
+        time2 = state2.json()['data']['leftDays']
+        time2 = time2.split('.')[0]
+        print(time2)
         if sever == 'on':
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess2+'，you have '+time2+' days left')
     else:
         requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
 
 def main_handler(event, context):
-  return start()
+  return start2()
 
 if __name__ == '__main__':
-    start()
+    start2()
 
     
